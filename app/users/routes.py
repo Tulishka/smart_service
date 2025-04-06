6ee2348e1f5204728ff83feaf3efce921ba6d668
ira <prijmenko.ira@mail.ru>
2025-04-06 21:13:47 +0700

    Добавлена тестовая ручка (/users) user_list.html

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,12 +1,22 @@
-from flask import Blueprint
-from markupsafe import escape
+from random import randint
+
+from flask import Blueprint, render_template, request, redirect, url_for
 
 from app import db
-from app.users.models import User
+from app.users.models import User, UserStatus
 
-bp = Blueprint('users', __name__, url_prefix='/users')
+bp = Blueprint("users", __name__, url_prefix="/users", template_folder="templates")
 
-@bp.route('/')
-def users_list():
+@bp.route("/", methods=["GET","POST"])
+def user_list():
+    if request.method == "POST":
+        user = User(
+            name=f"User {randint(1,100)}",
+            phone=f"8{randint(1000000000,1000000000)}",
+            status=UserStatus.ACTIVE,
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("users.user_list"))
     users = db.session.query(User).all()
-    return "Список пользователей " + escape(str(users))
+    return render_template("user_list.html", users=users)
