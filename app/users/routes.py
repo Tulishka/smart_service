70581e58197c9dd140df41e53e6a6e400f0d82cb
maxim <mamkamat.lenovo.x@gmail.com>
2025-04-07 22:08:42 +0700

    Убрана тестовая кнопка создания пользователей

--- app/users/routes.py
+++ app/users/routes.py
@@ -8,16 +8,7 @@ from app.users.models import User, UserStatus
 bp = Blueprint("users", __name__, url_prefix="/users", template_folder="templates")
 
 
-@bp.route("/", methods=["GET","POST"])
+@bp.route("/", methods=["GET"])
 def user_list():
-    if request.method == "POST":
-        user = User(
-            name=f"User {randint(1,100)}",
-            phone=f"8{randint(1000000000, 1000000000)}",
-            status=UserStatus.ACTIVE,
-        )
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for("users.user_list"))
     users = db.session.query(User).all()
     return render_template("user_list.html", users=users)
