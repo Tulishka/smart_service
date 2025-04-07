e3eea6745dd37a46c777257c8b5a2708e67b81d7
ira <prijmenko.ira@mail.ru>
2025-04-07 15:23:56 +0700

    Добавлены настройки login_manager (сообщение, что требуется залогиниться на русском)

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,8 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "auth.login"
+login_manager.login_message = "Требуется выполнить вход"
+login_manager.login_message_category = "info"
 
 with app.app_context():
     from app.database import all_models
