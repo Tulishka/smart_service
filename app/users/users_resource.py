6226c07edf405aa3909a151593e3c86555fbd71f
maxim <mamkamat.lenovo.x@gmail.com>
2025-04-19 10:52:14 +0700

    Добавлены два ресурса пользователя и организован метод get для списка пользователей

---
+++ app/users/users_resource.py
@@ -0,0 +1,35 @@
+from flask_restful import Resource, abort
+from flask import jsonify
+
+from app.users.models import User, Role, Department
+
+
+def abort_if_user_not_found(user_id):
+    users = User.query.filter_by(id=user_id).first()
+    if not users:
+        abort(404, message=F"User {user_id} not found")
+
+
+def user_to_dict(user):
+    return {
+        "id": user.id,
+        "name": user.name,
+        "department": Department.query.filter_by(id=user.department_id).first().name if user.department_id is not None
+        else "ОТСУТСТВУЕТ",
+        "roles": [role.name for role in user.roles],
+        "phone": user.phone,
+        "status": "ДОСТУПЕН" if user.status.name == "ACTIVE" else "НЕ ДОСТУПЕН"
+    }
+
+
+class UsersResource(Resource):
+    pass
+
+
+class UsersListResource(Resource):
+    def get(self):
+        users = list(map(lambda x: user_to_dict(x), User.query.all()))
+        return jsonify({"users": users})
+
+    def post(self):
+        pass
