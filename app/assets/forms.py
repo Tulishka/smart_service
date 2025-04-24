be97ea61edf6472a5ab63f9567395083ef8d05c7
maxim <mamkamat.lenovo.x@gmail.com>
2025-04-24 21:36:03 +0700

    Форма для добавления вида ассетов

---
+++ app/assets/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+import wtforms.fields.simple as field
+import wtforms.fields.choices as choices
+from wtforms.validators import DataRequired
+from wtforms import SelectMultipleField, widgets
+
+
+class AssetTypeForm(FlaskForm):
+    name = field.StringField("Название вида ассетов", validators=[DataRequired()])
+    description = field.StringField("Описание к виду ассетов", validators=[DataRequired()])
+    image = field.FileField(validators=[FileAllowed(['jpg', 'png'], "Картинка в jpg/png")])
+    submit = field.SubmitField("Добавить")
