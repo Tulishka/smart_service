6f7afe028add49a2f2ba8d158acb0427b9313e36
maxim <mamkamat.lenovo.x@gmail.com>
2025-04-24 19:31:16 +0700

    обработчики страниц с ассетами

--- app/assets/routes.py
+++ app/assets/routes.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-bp = Blueprint("assets", __name__, url_prefix="/assets")
+bp = Blueprint("assets", __name__, url_prefix="/assets", template_folder="templates")
 
 
 @bp.route("/")
 def index():
-    return "Главная страница Smart service!"
+    return render_template("assets_list.html")
+
+
+@bp.route("/types")
+def types():
+    return render_template("types_list.html")
