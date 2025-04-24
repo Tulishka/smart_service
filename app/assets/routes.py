3109001202082d8d69357ab75a86cad6eb52136f
maxim <mamkamat.lenovo.x@gmail.com>
2025-04-24 21:37:04 +0700

    Обработчик страницы с видами ассетов + отдельного вида ассетов (требуется фикс)

--- app/assets/routes.py
+++ app/assets/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from app.assets.models import AssetType
 
 bp = Blueprint("assets", __name__, url_prefix="/assets", template_folder="templates")
 
@@ -10,4 +11,11 @@ def index():
 
 @bp.route("/types")
 def types():
-    return render_template("types_list.html")
+    data = AssetType.query.all()
+    print(data[0].image)
+    return render_template("types_list.html", data=data)
+
+
+@bp.route("/type_show/<int:type_id>")
+def asset_type_show(type_id):
+    return render_template("types_show.html", type=type_id)
