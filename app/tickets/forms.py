cd2109ec29a5e9e17a37f524808f2ede73a39196
ira <prijmenko.ira@mail.ru>
2025-05-11 17:11:14 +0700

    Возможность оставить коммент при создании заявки

--- app/tickets/forms.py
+++ app/tickets/forms.py
@@ -1,14 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 from wtforms.fields.choices import RadioField
-from wtforms.fields.simple import StringField
-from wtforms.validators import DataRequired
+from wtforms.fields.simple import StringField, TextAreaField
+from wtforms.validators import DataRequired, Length, Optional
 
+from app.database import model_const as mc
 from app.tickets.models import TicketStatus, TicketResults
 
 
 class OptionForm(FlaskForm):
     option = RadioField('Что бы создать заявку выберите опцию', validators=[DataRequired()])
+    description = TextAreaField(
+        'Дополнительные сведения',
+        validators=[
+            Optional(),
+            Length(max=mc.DESCR_LEN, message=f"Максимальная длина {mc.DESCR_LEN} символов")
+        ]
+    )
     submit = SubmitField('Создать заявку')
 
 
