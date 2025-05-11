2779984927584379c0ec9d0c9e4f5ac094a28f3a
maxim <mamkamat.lenovo.x@gmail.com>
2025-05-11 20:35:13 +0700

    Тесты к юзер апи отформатированы в отсутствие куков

--- tests/users_api/put.py
+++ tests/users_api/put.py
@@ -2,14 +2,8 @@ from pprint import pprint
 import requests
 
 
-with requests.Session() as session:
-    # Вход в систему под данными первого пользователя
-    pprint(session.post("http://127.0.0.1:5000/api/login", json={"phone": "+222", "password": "12345"}).json())
-
-    print("--" * 20)
-
-    # Изменение департаммента второго пользователя на департамент с id=1 и пароля на 54321
-    print(session.put("http://127.0.0.1:5000/api/users/2", json={
-        "department_id": 2,
-        "password": "12345"
-    }).json())
+# Изменение департаммента третьего пользователя на департамент с id=3 и пароля на 12345
+print(requests.put("http://127.0.0.1:5000/api/users/3", json={
+    "department_id": 2,
+    "password": "12345"
+}).json())
