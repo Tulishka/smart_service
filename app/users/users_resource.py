eb43bfbec0ab94a720cf2139209cc7684e569e1a
maxim <mamkamat.lenovo.x@gmail.com>
2025-05-11 22:53:33 +0700

    Проверка по ключу + тесты по ключу + проверка на существование юзера с данным номером

--- app/users/users_resource.py
+++ app/users/users_resource.py
@@ -1,12 +1,28 @@
 from flask_restful import Resource, abort
-from flask import jsonify
+from flask import jsonify, request
 
 from app.users.models import User, Role, Department, users_roles
 from app.users.parsers import standart_parser, unrequired_parser, login_parser
 
+from functools import wraps
+
 from flask_login import current_user, login_user
 
 from app import db
+
+from app.config import Config
+
+
+def api_key_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        apikey = request.args.get('apikey')
+        if not apikey:
+            abort(401, message="API key is missing")
+        if apikey not in Config.USERS_API_KEYS:
+            abort(403, message="Invalid API key")
+        return f(*args, **kwargs)
+    return decorated
 
 
 def user_or_404(user_id, get_user_obj=False):
@@ -16,6 +32,11 @@ def user_or_404(user_id, get_user_obj=False):
     if get_user_obj:
         return user
     return user_to_dict(user)
+
+
+def abort_if_user_exists(phone):
+    if User.query.filter(User.phone == phone).all():
+        abort(409, message="User with this phone already exists")
 
 
 def user_to_dict(user):
@@ -38,10 +59,12 @@ def set_password_or_400(user, password):
 
 
 class UsersResource(Resource):
+    @api_key_required
     def get(self, user_id):
         user = user_or_404(user_id)
         return jsonify({"users": user})
 
+    @api_key_required
     def delete(self, user_id):
         user = user_or_404(user_id, get_user_obj=True)
 
@@ -51,6 +74,7 @@ class UsersResource(Resource):
         db.session.commit()
         return jsonify({"success": "OK"})
 
+    @api_key_required
     def put(self, user_id):
         user = user_or_404(user_id, get_user_obj=True)
         args = unrequired_parser.parser.parse_args()
@@ -63,6 +87,9 @@ class UsersResource(Resource):
         user.name = new_user_data.get("name", user.name)
         user.phone = new_user_data.get("phone", user.phone)
 
+        if "phone" in new_user_data.keys():
+            abort_if_user_exists(new_user_data["phone"])
+
         if "department_id" in new_user_data.keys():
             if Department.query.get(new_user_data["department_id"]):
                 user.department_id = new_user_data["department_id"]
@@ -74,12 +101,16 @@ class UsersResource(Resource):
 
 
 class UsersListResource(Resource):
+    @api_key_required
     def get(self):
         users = list(map(lambda x: user_to_dict(x), User.query.all()))
         return jsonify({"users": users})
 
+    @api_key_required
     def post(self):
         args = standart_parser.parser.parse_args()
+        abort_if_user_exists(args["phone"])
+
         user = User()
 
         user.status = "ACTIVE"
--- tests/users_api/put.py
+++ tests/users_api/put.py
@@ -3,7 +3,8 @@ import requests
 
 
 # Изменение департаммента третьего пользователя на департамент с id=3 и пароля на 12345
-print(requests.put("http://127.0.0.1:5000/api/users/3", json={
-    "department_id": 2,
-    "password": "12345"
-}).json())
+print(requests.put(
+    "http://127.0.0.1:5000/api/users/3?apikey=CD3rlxIXQ0pGVrE72r1n33MBhH1Q5I4xd2eTgHOxBbq9HTN5BdVVZ4c3gfgNlY0",
+    json={
+        "department_id": 2,
+        "password": "12345"}).json())
