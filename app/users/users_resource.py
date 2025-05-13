1ccb4f78290aedd77635a72238b6a5d2514db919
maxim <mamkamat.lenovo.x@gmail.com>
2025-05-13 12:45:24 +0700

    Добавлены докстринги и комментарии к ресурсу пользователей + функция проверки наличия отдела в системе

--- app/users/users_resource.py
+++ app/users/users_resource.py
@@ -1,3 +1,11 @@
+"""
+Модуль для работы с ресурсом API пользователей.
+
+Содержит классы:
+- UsersListResource: Работа с пользователями в целом (get, post запросы)
+- UsersResource: Работа с отдельными пользователями по id (get, put, delete запросы)
+"""
+
 from flask_restful import Resource, abort
 from flask import jsonify, request
 
@@ -14,11 +22,19 @@ from app.config import Config
 
 
 def api_key_required(f):
+    """Декоратор для проверки корректности ключа API
+
+    :param f: Входящая функция
+    :return: Результат функции после проверки ключа API
+    """
     @wraps(f)
     def decorated(*args, **kwargs):
         apikey = request.args.get('apikey')
+        # Случай отсутствия ключа API в query параметрах
         if not apikey:
             abort(401, message="API key is missing")
+
+        # Случай передачи неверного ключа API в параметрах
         if apikey not in Config.USERS_API_KEYS:
             abort(403, message="Invalid API key")
         return f(*args, **kwargs)
@@ -26,6 +42,12 @@ def api_key_required(f):
 
 
 def user_or_404(user_id, get_user_obj=False):
+    """Функция поиска пользователя по id и возвращающая ошибку в случае неудачи
+
+    :param user_id: Id нужного пользователя
+    :param get_user_obj: Что возвращает функция (False - словарь с данными пользователя, True - объект)
+    :return: (Словарь/объект пользователя)
+    """
     user = User.query.get(user_id)
     if not user:
         abort(404, message=F"User {user_id} not found")
@@ -35,11 +57,25 @@ def user_or_404(user_id, get_user_obj=False):
 
 
 def abort_if_user_exists(phone):
+    """Функция проверки существования пользователя с данным номером телефона
+
+    :param phone: Номер телефона пользователя
+    """
+    # Возвращаем ошибку 409 (Conflict), если пользователь с таким номером уже существует
     if User.query.filter(User.phone == phone).all():
         abort(409, message="User with this phone already exists")
 
 
 def user_to_dict(user):
+    """Функция преобразования объекта пользователя в словарь
+
+    :param user: Объект пользователя
+    :return: Словарь с данными о пользователе для вывода в формате:
+            id: Уникальный идентификатор пользователя
+            name: Имя пользователя
+            department: Отдел пользователя (Если имеется)
+            roles Роли пользователя
+    """
     return {
         "id": user.id,
         "name": user.name,
@@ -52,20 +88,50 @@ def user_to_dict(user):
 
 
 def set_password_or_400(user, password):
+    """Функция проверки корректности длины пароля и устанавливающая его в таком случае
+
+    :param user: Объект пользователя
+    :param password: Пароль для установки
+    :return: Ошибка / None
+    """
     if len(password) >= 5:
         user.set_password(password)
     else:
         abort(400, message=F"the password length must be >= 5")
 
 
+def put_department_or_409(user, department):
+    """Функция проверки существования департамента и устанавливающаяв его в таком случае
+
+    :param user:
+    :param department:
+    :return: Ошибка / None
+    """
+    if Department.query.get(department):
+        user.department_id = department
+    else:
+        abort(409, message="Department with this id is not exist")
+
+
 class UsersResource(Resource):
+    """Класс для реализации работы API запросов к конкретному пользователю через его id"""
     @api_key_required
     def get(self, user_id):
+        """GET-запрос для получения информации о пользователе в формате JSON
+
+        :param user_id: Уникальный идентификтор пользователя
+        :return: JSON файл с информацией о пользователе/ошибке
+        """
         user = user_or_404(user_id)
         return jsonify({"users": user})
 
     @api_key_required
     def delete(self, user_id):
+        """DELETE-запрос для удаления пользователя (Во избежание ошибок нужно использовать с осторожностью!)
+
+        :param user_id: Уникальный идентификатор пользователя
+        :return: Уведеомление об успешном удалении/ошибке
+        """
         user = user_or_404(user_id, get_user_obj=True)
 
         users_roles.delete().where(users_roles.c.user_id == user_id)
@@ -76,10 +142,17 @@ class UsersResource(Resource):
 
     @api_key_required
     def put(self, user_id):
+        """PUT-запрос для изменения пользователя
+
+        :param user_id: Уникальный идентификатор пользователя
+        :return: Уведомление об успешном изменении пользователя/ошибке
+        """
+
+        # Получение пользователя и аргументов, на которые есть необходимость изменить существующие
         user = user_or_404(user_id, get_user_obj=True)
         args = unrequired_parser.parser.parse_args()
 
-        new_user_data = {}
+        new_user_data = {}  # Словарь с данными, которые необходимо изменить в объекте пользователя
         for key, value in args.items():
             if value is not None:
                 new_user_data[key] = value
@@ -88,34 +161,46 @@ class UsersResource(Resource):
         user.phone = new_user_data.get("phone", user.phone)
 
         if "phone" in new_user_data.keys():
-            abort_if_user_exists(new_user_data["phone"])
+            abort_if_user_exists(new_user_data["phone"])  # Проверка уникальности телефона
 
         if "department_id" in new_user_data.keys():
-            if Department.query.get(new_user_data["department_id"]):
-                user.department_id = new_user_data["department_id"]
+            put_department_or_409(user, new_user_data["department_id"])
 
         if "password" in new_user_data.keys():
             set_password_or_400(user, new_user_data["password"])
+
         db.session.commit()
         return jsonify({"success": "OK"})
 
 
 class UsersListResource(Resource):
+    """Класс для реализации общих API запросов к пользователям"""
     @api_key_required
     def get(self):
+        """GET-запрос для получения информации о всех пользователях в формате JSON
+
+        :return: JSON файл об информации о всех пользователях / ошибке
+        """
         users = list(map(lambda x: user_to_dict(x), User.query.all()))
         return jsonify({"users": users})
 
     @api_key_required
     def post(self):
+        """POST-запрос для регистрации пользователя в системе
+
+        :return: Id только что добавлено пользователя в случае успешного добавления / ошибку
+        """
         args = standart_parser.parser.parse_args()
         abort_if_user_exists(args["phone"])
 
         user = User()
-
         user.status = "ACTIVE"
         user.name = args["name"]
         user.phone = args["phone"]
+
+        if "department_id" in args.keys():
+            put_department_or_409(user, new_user_data["department_id"])
+
         set_password_or_400(user, args["password"])
         db.session.add(user)
         db.session.commit()
